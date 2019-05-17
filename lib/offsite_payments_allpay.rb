2ee3f9d3c6d6f8683c9348ee5b38613bf2b8b0c7
stan <solve153@gmail.com>
2019-05-17 16:48:16 +0800

    Initial

---
+++ lib/offsite_payments_allpay.rb
@@ -0,0 +1,6 @@
+require "offsite_payments_allpay/version"
+
+module OffsitePaymentsAllpay
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/offsite_payments_allpay/version.rb
@@ -0,0 +1,3 @@
+module OffsitePaymentsAllpay
+  VERSION = "0.1.0"
+end
